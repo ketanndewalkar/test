87a9bd4c94a3623d9688483611e33e5232e65b72
Suyash Singh <suyashsingh.mit@gmail.com>
2026-02-22 23:30:55 +0530

    Implemented consolelog 6 (#4)

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 console.log("hello");
 console.log("hello1");
 console.log("hello2");
+console.log("hello2");
+console.log("hello2");
+console.log("hello3");
+console.log("hello4");
+console.log("hello5");
+console.log("hello6");
